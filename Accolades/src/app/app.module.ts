e6018273fb73eb5c592bc567fb582d3fd6813314
okwueze <69356352+okwueze@users.noreply.github.com>
2020-10-11 17:40:37 +0100

    Edits

--- Accolades/src/app/app.module.ts
+++ Accolades/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component'; 
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ Accolades/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  mobileMenu = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleSidebar(){
+    this.mobileMenu = !this.mobileMenu
+  } // The exclamation make the function do the opposite of what it is
+  // meaning that as mobileMenu is set to false it will make it true and vice versa which will make the
+  // the navbar slide in and out
+
+}
